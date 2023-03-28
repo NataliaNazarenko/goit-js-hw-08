c5a660436048eaced591101d9460ead0cd349459
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-03-28 22:29:39 +0300

    add solving 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', _throttle(onPlayer, 1000));
+
+function onPlayer(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
