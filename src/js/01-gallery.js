69ed897b04b04e66092e0aa66f1fef24d5e18f4d
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-03-28 09:55:28 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const galleryList = document.querySelector('.gallery');
 console.log(galleryItems);
