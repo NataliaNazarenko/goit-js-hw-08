3d4ffc1cbec4e38796ce47ea0e9e1b5e3e04ed3f
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-03-28 22:47:48 +0300

    fixed 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,10 +6,11 @@ const player = new Player(iframe);
 
 const CURRENT_TIME = 'videoplayer-current-time';
 
-player.on('timeupdate', _throttle(onPlayer, 1000));
-
-function onPlayer(event) {
-  localStorage.setItem(CURRENT_TIME, event.seconds);
-}
+player.on(
+  'timeupdate',
+  _throttle(event => {
+    localStorage.setItem(CURRENT_TIME, event.seconds);
+  }, 1000)
+);
 
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
