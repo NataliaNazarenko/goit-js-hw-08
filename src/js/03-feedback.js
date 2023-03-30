858f015590e4c0537ed95da26b1fca00fb0229c4
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-03-30 21:44:06 +0300

    add solving 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]');
+const textarea = document.querySelector('[name="message"]');
+const FEEDBACK_FORM = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onGetDataFrom, 1000));
+form.addEventListener('submit', onClickBtn);
+
+addLocal();
+const dataFrom = {};
+
+function onGetDataFrom(event) {
+  dataFrom[event.target.name] = event.target.value;
+
+  saveLocal(FEEDBACK_FORM, dataFrom);
+}
+
+function saveLocal(key, value) {
+  try {
+    const data = JSON.stringify(value);
+    localStorage.setItem(key, data);
+  } catch (err) {
+    console.log('Stringify error');
+  }
+}
+
+function loadLocal(key) {
+  try {
+    const data = localStorage.getItem(key);
+    return data === null ? undefined : JSON.parse(data);
+  } catch (err) {
+    console.log('Stringify error');
+  }
+}
+function addLocal(event) {
+  const currentData = loadLocal(FEEDBACK_FORM);
+
+  if (currentData) {
+    input.value = currentData.email;
+    textarea.value = currentData.message;
+  }
+}
+
+function onClickBtn(event) {
+  event.preventDefault();
+  console.log(loadLocal(FEEDBACK_FORM));
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM);
+}
