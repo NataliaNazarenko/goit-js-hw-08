be727abf88d3645d4b236340d706198475a05e39
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-04-03 20:35:37 +0300

    add inline style task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,13 +6,16 @@ import { galleryItems } from './gallery-items';
 
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
+const liElement = document.querySelector('.gallery__item');
 
 const makeGalleryMarkup = ({ preview, original, description }) => {
   return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`;
 };
 
 const makeGalleryMarkupEl = galleryItems.map(makeGalleryMarkup).join('');
+
 galleryList.insertAdjacentHTML('beforeend', makeGalleryMarkupEl);
+galleryList.style.listStyle = 'none';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
